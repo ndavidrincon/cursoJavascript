c18364407449d291cb949587d09b35cdedc0910e
ndavidrincon <digitalonly.agency@gmail.com>
2023-01-18 12:57:00 -0500

    integracion de la barra de navegacion en modo mobile

--- main.js
+++ main.js
@@ -1,8 +1,17 @@
 const menuEmail = document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu");
+const burgerIcon = document.querySelector(".menu");
+const mobileMenu = document.querySelector(".mobile-menu");
+
 
 menuEmail.addEventListener("click", toggleDesktopMenu);
+burgerIcon.addEventListener("click", toggleMobileMenu);
 
 function toggleDesktopMenu () {
     desktopMenu.classList.toggle("inactive");
+}
+
+function toggleMobileMenu () {
+    console.log("click")
+    mobileMenu.classList.toggle("inactive");
 }
