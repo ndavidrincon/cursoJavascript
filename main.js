54c6d8dafa7163895dea956105895e2811a00d27
ndavidrincon <digitalonly.agency@gmail.com>
2023-01-18 16:13:31 -0500

    Interactividad y optimizacion del carrito de compras con los menus de desktop y mobile

--- main.js
+++ main.js
@@ -1,17 +1,40 @@
 const menuEmail = document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu");
+const menuCarritoIcon = document.querySelector(".navbar-shopping-cart")
 const burgerIcon = document.querySelector(".menu");
 const mobileMenu = document.querySelector(".mobile-menu");
+const aside = document.querySelector(".product-detail")
 
 
 menuEmail.addEventListener("click", toggleDesktopMenu);
 burgerIcon.addEventListener("click", toggleMobileMenu);
+menuCarritoIcon.addEventListener("click", toggleCarritoAside);
 
 function toggleDesktopMenu () {
+    const isMenuCarritoIconClosed = menuCarritoIcon.classList.contains("inactive");
+
+    if (!isMenuCarritoIconClosed) {
+        aside.classList.add("inactive")
+    };
+    
     desktopMenu.classList.toggle("inactive");
 }
 
 function toggleMobileMenu () {
-    console.log("click")
+    const isMenuCarritoIconClosed = menuCarritoIcon.classList.contains("inactive");
+
+    if (!isMenuCarritoIconClosed) {
+        aside.classList.add("inactive")
+    }
+
     mobileMenu.classList.toggle("inactive");
+}
+
+function toggleCarritoAside () {
+    const isMobileMenuClosed = mobileMenu.classList.contains("inactive");
+
+    if (!isMobileMenuClosed) {
+        mobileMenu.classList.add("inactive")
+    };
+    aside.classList.toggle("inactive");
 }
