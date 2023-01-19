ffcc065fbed55f2bd54b090859e57ab9596835dc
ndavidrincon <digitalonly.agency@gmail.com>
2023-01-19 07:11:14 -0500

    Maquetando en JS la seccion de product cards de html (tiene errores)

--- main.js
+++ main.js
@@ -3,8 +3,8 @@ const desktopMenu = document.querySelector(".desktop-menu");
 const menuCarritoIcon = document.querySelector(".navbar-shopping-cart")
 const burgerIcon = document.querySelector(".menu");
 const mobileMenu = document.querySelector(".mobile-menu");
-const aside = document.querySelector(".product-detail")
-
+const aside = document.querySelector(".product-detail");
+const cardsContainer = document.querySelector(".cards-container");
 
 menuEmail.addEventListener("click", toggleDesktopMenu);
 burgerIcon.addEventListener("click", toggleMobileMenu);
@@ -16,7 +16,7 @@ function toggleDesktopMenu () {
     if (!isMenuCarritoIconClosed) {
         aside.classList.add("inactive")
     };
-    
+
     desktopMenu.classList.toggle("inactive");
 }
 
@@ -38,3 +38,69 @@ function toggleCarritoAside () {
     };
     aside.classList.toggle("inactive");
 }
+
+const productList = [];
+productList.push ({
+    name: "Bike",
+    price: 120,
+    image: "https://images.pexels.com/photos/100582/pexels-photo-100582.jpeg?auto=compress&cs=tinysrgb&w=600"
+});
+
+productList.push ({
+    name: "Tv",
+    price: 300,
+    image: "https://images.pexels.com/photos/100582/pexels-photo-100582.jpeg?auto=compress&cs=tinysrgb&w=600"
+});
+
+productList.push ({
+    name: "Compu",
+    price: 620,
+    image: "https://images.pexels.com/photos/100582/pexels-photo-100582.jpeg?auto=compress&cs=tinysrgb&w=600"
+});
+
+function renderProducts (arr) {
+    for (product of arr) {
+        console.log("entro a la funcion")
+        // Nos ayuda a crear cada uno de nuestros elementos de html pero usando js
+        const productCard = document.createElement("div"); // Estoy creando el div donde va a quedar la clase product-card
+        productCard.classList.add("product-card");
+    
+        // product= {name, price, image} -> product.image
+        const productImg = document.createElement("img");
+        productImg.setAttribute("src", product.image);
+    
+        const productInfo = document.createElement("div");
+        productInfo.classList.add("product-info");
+    
+        
+        const productInfoDiv = document.createElement("div");
+        
+        const productPrice = document.createElement("p");
+        productPrice.innerText = "$" + product.price; 
+        const productName = document.createElement("p");
+        productName.innerText = product.name;
+        productInfoDiv.appendChild(productPrice);
+        productInfoFigure.appendChild(productName);
+        
+        productInfoDiv.appendChild(productPrice);
+        productInfoDiv.appendChild(productName);
+    
+        const productInfoFigure = document.createElement("figure");
+        const productImgCart = document.createElement("img");
+
+        img.setAttribute("src", "./icons/bt_add_to_cart.svg");
+    
+        productInfoFigure.appendChild(productImgCart);
+    
+        productInfo.appendChild(productInfoDiv);
+        productInfo.appendChild(productInfoFigure);
+    
+        // tenemos que insertar productInfo y ProductImage a dentro de productCart
+        productCard.appendChild(productImg);
+        productCard.appendChild(productInfo);
+    
+        cardsContainer.appendChild(productCard);
+    };
+}
+
+renderProducts(productList)
